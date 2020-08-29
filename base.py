110cf7aaf906d1a15a8d42f6fbc88e22ac5d1f3e
Matt Barnard <matt@serverus.co.uk>
2020-08-29 18:46:07 +0100

    Completed

--- base.py
+++ base.py
@@ -8,7 +8,49 @@ import csv
 TOKEN = config('VIMEO_TOKEN')
 CLIENT_ID = config('VIMEO_CLIENT_ID')
 SECRET = config('VIMEO_SECRET')
-USER = config('VIMEO_USER',cast=int)
+USER = config('VIMEO_USER', cast=int)
+
+
+def put_video():
+    pass
+
+
+def set_privacy_and_embed():
+    print('Updating privacy settings for all cached videos')
+    for id in get_list_of_vid_ids():
+        print('Editing {}'.format(id))
+        uri = 'https://api.vimeo.com/videos/{}'.format(id)
+        client = get_client()
+
+        json_data = {
+            'privacy':{
+                'view':'disable',
+                'embed':'whitelist',
+                'download':False,
+                'comments':'nobody'
+            },
+        }
+        response = client.patch(uri, json=json_data)
+        if response.status_code == 200:
+            print('Edit successful')
+        else:
+            print('Edit fail: {}'.format(str(response.status_code)))
+
+
+def add_whitelist_domain():
+    DOMAIN = 'videos.britishphenomenology.org.uk'
+
+    print('Adding domain to whitelist ({})'.format(DOMAIN))
+    for id in get_list_of_vid_ids():
+        print('Editing {}'.format(id))
+        uri = 'https://api.vimeo.com/videos/{}/privacy/domains/{}'.format(id, DOMAIN)
+        client = get_client()
+        response = client.put(uri)
+        if response.status_code == 204:
+            print('Edit successful')
+        else:
+            print('Edit fail: {}. Reason: {}'.format(str(response.status_code)), response.reason)
+
 
 def get_client():
     return vimeo.VimeoClient(
@@ -17,23 +59,25 @@ def get_client():
         secret='{}'.format(SECRET),
     )
 
+
 def get_total_pages_from_data(data):
     last_uri = data['paging']['last']
     total_pages = int(last_uri[-1])
     return total_pages
 
-def get_list_of_pages(total, uri):
 
+def get_list_of_pages(total, uri):
     uri_list = []
-    for x in range(2,total+1):
-        uri_list.append(uri+'?page={}'.format(str(x)))
+    for x in range(2, total + 1):
+        uri_list.append(uri + '?page={}'.format(str(x)))
     return uri_list
+
 
 def get_list_of_all_videos(fields=None):
     client = get_client()
     uri = 'https://api.vimeo.com/me/videos'.format(USER)
     if fields is not None:
-        response = client.get(uri,params={"fields": "{}".format(fields)})
+        response = client.get(uri, params={"fields": "{}".format(fields)})
     else:
         response = client.get(uri)
 
@@ -44,7 +88,7 @@ def get_list_of_all_videos(fields=None):
     total_pages = get_total_pages_from_data(json_data)
 
     if total_pages > 1:
-        for url in get_list_of_pages(total_pages,uri):
+        for url in get_list_of_pages(total_pages, uri):
             if fields is not None:
                 response = client.get(url, params={"fields": "{}".format(fields)})
             else:
@@ -55,17 +99,17 @@ def get_list_of_all_videos(fields=None):
     rows = []
     for js in json_data_set:
         for jrow in js:
-
-            rows.append([jrow['uri'].lstrip('/videos/'),jrow['name'],jrow['description']])
+            rows.append([jrow['uri'].lstrip('/videos/'), jrow['name'], jrow['description']])
 
     return rows
 
-def save_csv(list, fname):
 
+def save_csv(list, fname):
     with open('{}.csv'.format(fname), 'w') as csvfile:
         writer = csv.writer(csvfile, dialect='excel')
         for row in list:
             writer.writerow(row)
+
 
 def load_csv(fname):
     with open('{}.csv'.format(fname), 'r') as csvfile:
@@ -76,16 +120,19 @@ def load_csv(fname):
 
 def get_vids_csv():
     vid_list = get_list_of_all_videos()
-    save_csv(vid_list,'videos')
+    save_csv(vid_list, 'videos')
     return True
+
 
 def start():
     print("Started")
+
 
 def get_list_of_vid_ids():
     vids = load_csv('videos')
     id_list = [row[0] for row in vids]
     return id_list
+
 
 def build_embeded_html_files():
     id_list = get_list_of_vid_ids()
@@ -99,15 +146,16 @@ def build_embeded_html_files():
 </html>
     """
     for id in id_list:
-        with open('html/'+id+'.html','w') as f:
+        with open('html/' + id + '.html', 'w') as f:
             f.write(template.format(id))
-
 
 
 def init_funcs():
     funcs = {
         "--get-vids-csv": get_vids_csv,
         "--build-html": build_embeded_html_files,
+        "--set-privacy": set_privacy_and_embed,
+        "--set-domain": add_whitelist_domain,
     }
     return funcs
 
